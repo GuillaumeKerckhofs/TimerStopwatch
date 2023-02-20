74efe745a42d0bd2c86f7f137ec12b0f5617a5db
Guillaume Kerckhofs <guillaume.kerckhofs@student.umons.ac.be>
2023-02-20 20:59:44 +0100

    test: test setTimer methods

---
+++ src/test/java/states/setTimerTest.java
@@ -0,0 +1,33 @@
+package states;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import states.timer.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class setTimerTest {
+
+    private Context context;
+
+    @BeforeEach
+    public void setUp() {
+        // reset the initial values of timer to avoid inferences between different consecutive tests
+        context = new Context();
+        context.currentState = SetTimer.Instance(); // because we are testing the IdleTimer state here...
+        AbstractTimer.resetInitialValues();
+    }
+
+    @Test
+    public void testSetTimer(){
+
+        context.left();
+        context.tick();
+        assertEquals(AbstractTimer.getMemTimer(),1);
+
+        context.up();
+        context.tick();
+        assertEquals(AbstractTimer.getMemTimer(),7);
+    }
+}
